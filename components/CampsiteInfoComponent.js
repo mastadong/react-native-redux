078bf60d882e2c19a70b87f9f0d9264643e72334
noahb <noahb@emeraldaire.com>
2020-12-11 14:19:37 -0800

    Added Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList,
          Modal, Button, StyleSheet,
-        Alert, PanResponder } from 'react-native';
+        Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { postFavorite, postComment } from '../redux/ActionCreators';
 import { connect } from 'react-redux';
@@ -172,6 +172,16 @@ function RenderCampsite(props) {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    };
+
 
     if (campsite) {
         return (
@@ -204,6 +214,14 @@ function RenderCampsite(props) {
                             raised
                             reverse
                             onPress={()=> props.onShowModal()}
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
                         />
                     </View>
                 </Card>
